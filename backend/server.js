5ac2906936263b281e0cb60af1830b39d1e6fa11
Fatih Karaca <98697501+F-Krc@users.noreply.github.com>
2023-08-10 15:38:50 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const connectionString = process.env.MONGO_URL;
 app.use(express.json());
 app.use(
   cors({
-    origin: ['https://user-app-two-liard.vercel.app/'],
+    origin: ['user-app-frontend-flax.vercel.app/'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
   })
